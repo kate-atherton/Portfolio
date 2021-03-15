46d11a09b50175d994f0ca2f532d23bddb7057d8
kate Atherton <kateatherton222@gmail.com>
2021-03-15 14:04:55 +0000

    sets up project files and structure

---
+++ src/js/controller.js
@@ -0,0 +1,2 @@
+import "../sass/main.scss";
+import * as model from "./model.js";
---
+++ webpack.config.js
@@ -0,0 +1,104 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/js/controller.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    host: "0.0.0.0",
+    port: 8080,
+    contentBase: "./static",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+            presets: [
+              "@babel/preset-env",
+              // {
+              //   targets: [
+              //     "last 3 Chrome versions",
+              //     "last 3 Firefox versions",
+              //     "last 3 edge versions",
+              //     "last 3 Safari versions",
+              //   ],
+              // },
+            ],
+            plugins: [
+              ["@babel/plugin-proposal-class-properties"],
+              ["@babel/plugin-transform-runtime"],
+            ],
+          },
+        },
+      },
+
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "images",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "fonts",
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundle.css",
+    }),
+    new webpack.HotModuleReplacementPlugin({
+      // Options...
+    }),
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+};
